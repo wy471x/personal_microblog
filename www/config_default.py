b80da67f7c8a5e3742cda2ab2ec19db8b910c594
wy471x <1004963485@qq.com>
2020-01-31 00:39:16 +0800

    newest update

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+__author__ = 'Dunk Wan'
+
+configs = {
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www_data',
+        'password': 'www_data',
+        'db': 'awesome'
+    },
+    'session':{
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+__author__ = 'Dunk Wan'
+
+configs = {
+    'db':{
+        'host': '127.0.0.1'
+    }
+}
